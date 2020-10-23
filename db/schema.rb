84ba4659f5aef1c0990e99da672132fe24c3a7b8
堀百花 <momoka@mbp-2.local>
2020-10-23 21:33:13 +0900

    add_user_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_030149) do
+ActiveRecord::Schema.define(version: 2020_10_23_111858) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_10_23_030149) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
