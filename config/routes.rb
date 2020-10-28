792808cb23d4bf50a702f535781d62c0ef8bffa0
堀百花 <momoka@mbp-2.local>
2020-10-28 22:09:24 +0900

    add like function

--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,9 @@ Rails.application.routes.draw do
   post "logout" => "users#logout"
   get "logout" => "users#logout"
 
+
+  # <----- Like ----->
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
+
 end
